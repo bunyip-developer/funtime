30fe28d76327766ddf2b82573f9c30eadfda7f83
Dave Oliver <big.friendly.daveoliver@gmail.com>
2020-01-05 10:18:03 +0000

    Added class script. Moved directories.

---
+++ basics/classes.py
@@ -0,0 +1,19 @@
+"""
+Example of how to use a function with documentation
+"""
+
+
+def func1(x):
+    """
+    Print x
+    :param x:
+    :return:
+    """
+    print(x)
+
+def func2(x):
+    print('output;', x)
+
+
+if __name__ == '__main__':
+    func1('xyz')
